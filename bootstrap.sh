0c5defddf48f855fe6a203c624272b5ded406756
Rajat Khandelwal <rajatgupta59@gmail.com>
2018-04-16 13:56:33 +0530

    native/docker option added

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,14 @@
 apt-get -y update
-apt-get -y install -y git wget curl telnet
-git clone https://github.com/prongs/yarn-azure.git
-cd yarn-azure
-bash run.sh $@
+DEPLOYMENT_METHOD=$1 # docker/native
+shift
+if [ $DEPLOYMENT_METHOD == "docker" ] then;
+	apt-get -y install docker
+	IMAGE=$1
+	shift
+	docker pull $IMAGE
+else
+	apt-get -y install -y git wget curl telnet
+	git clone https://github.com/prongs/yarn-azure.git
+	cd yarn-azure
+	bash run.sh $@
+fi
