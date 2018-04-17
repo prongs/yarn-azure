84550600a5ebf4d380916cbd74a431c796c7476b
Rajat Khandelwal <rajatgupta59@gmail.com>
2018-04-17 18:11:49 +0530

    single boostrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,16 @@ shift
 if [ $DEPLOYMENT_METHOD == "docker" ]; then
 	apt-get -y install docker.io
 	shift
-	bash run_with_docker.sh $@
+	for i in "$@"
+	do
+   		DOCKER_ENV_ARGS="$DOCKER_ENV_ARGS -e $i"
+   		eval "export $i"
+	done
+	IMAGE="hadoop-azure-"$1
+	docker login $DOCKER_REGISTRY_URL -u DOCKER_REGISTRY_USERNAME -p DOCKER_REGISTRY_PASSWORD
+
+	docker pull $IMAGE
+	docker run -itd $DOCKER_ENV_ARGS $IMAGE
 else
 	apt-get -y install -y git wget curl telnet
 	git clone https://github.com/prongs/yarn-azure.git
