cbf103ac3ec280a471400d7075bc8c6806bb2138
Rajat Khandelwal <rajatgupta59@gmail.com>
2018-04-17 18:25:31 +0530

    fixed docker bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,16 @@ DEPLOYMENT_METHOD=$1 # docker/native
 shift
 if [ $DEPLOYMENT_METHOD == "docker" ]; then
 	apt-get -y install docker.io
+	IMAGE="hadoop-azure-"$1
 	shift
 	for i in "$@"
 	do
    		DOCKER_ENV_ARGS="$DOCKER_ENV_ARGS -e $i"
    		eval "export $i"
 	done
-	IMAGE="hadoop-azure-"$1
-	docker login $DOCKER_REGISTRY_URL -u DOCKER_REGISTRY_USERNAME -p DOCKER_REGISTRY_PASSWORD
-
-	docker pull $IMAGE
-	docker run -itd $DOCKER_ENV_ARGS $IMAGE
+	docker login $DOCKER_REGISTRY_URL -u $DOCKER_REGISTRY_USERNAME -p $DOCKER_REGISTRY_PASSWORD
+	docker pull $DOCKER_REGISTRY_URL/$IMAGE
+	docker run -itd $DOCKER_ENV_ARGS $DOCKER_REGISTRY_URL/$IMAGE
 else
 	apt-get -y install -y git wget curl telnet
 	git clone https://github.com/prongs/yarn-azure.git
